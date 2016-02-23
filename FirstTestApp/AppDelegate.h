d0748a3b3fdbf6c430417ec72999e32dbba46723
Antonio Curci <antonio.curci@gmail.com>
2016-02-23 11:25:06 +0100

    Initial Commit

---
+++ FirstTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstTestApp
+//
+//  Created by Antonio Curci on 23/02/16.
+//  Copyright © 2016 MelMedtronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
